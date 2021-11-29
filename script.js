6a1b659ec686804f7d3a5660956ce3b9a893f494
jawbreaker666 <andrasev96@web.de>
2021-11-29 14:52:11 +0100

    addquestions

---
+++ script.js
@@ -0,0 +1,32 @@
+let questions = [{
+
+    "question": "An welcher Sprache orientieren sich die meisten Programmiersprachen?",
+    "answer_1": "englisch",
+    "answer_2": "deutsch",
+    "answer_3": "spanisch",
+    "answer_4": "französisch",
+    "right_answer": 1
+},
+
+{
+
+    "question": "Was bedeuten die Zahlen 1 und 0 in der Informatik?",
+    "answer_1": "links/rechts",
+    "answer_2": "oben/unten",
+    "answer_3": "wahr/falsch",
+    "answer_4": "ja/nein",
+    "right_answer": 3
+},
+
+
+{
+
+    "question": "Welche Textausgabe lernt man in den meisten Programmiersprachen als erstes?",
+    "answer_1": "New Webseite",
+    "answer_2": "Hello World",
+    "answer_3": "Welcome",
+    "answer_4": "Under construction",
+    "right_answer": 2
+},
+
+];
